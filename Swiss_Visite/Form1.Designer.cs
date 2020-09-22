b39e2279642507c5d15b4cc7f258f975f1f6eff6
aballa <aballa@ST214>
2020-09-22 15:44:16 +0200

    deuxieme

--- Swiss_Visite/Form1.Designer.cs
+++ Swiss_Visite/Form1.Designer.cs
@@ -45,7 +45,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(625, 342);
+            this.button1.Location = new System.Drawing.Point(452, 134);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
